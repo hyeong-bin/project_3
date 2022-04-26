1c46ccda7dbcbb56c804c1f5794840f691d8b7d5
hyeong-bin <heyongbinjo@gmail.com>
2022-04-26 15:06:56 -0700

    Add file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Numbers from './components/Numbers';
+import { useState } from 'react';
 
 function App() {
+  const [displayFact, setDisplayFact] = useState(false)
+  const [numberInput, setNumberInput] = useState('')
+
+const handleChangeInput=(event)=>{
+  setNumberInput(event.target.value)
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Random facts about a number</h1>
       </header>
+      <main>
+        <div>
+          <div>
+            Type a number:
+            <input value={numberInput} onChange={handleChangeInput}/>
+            <button>Clear</button>
+          </div>         
+          <Numbers displayFact={displayFact} numberInput={numberInput}/>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Numbers.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Numbers = ({displayFact, numberInput}) => {
+    const [fact, setFact] = useState('')
+
+    useEffect(() => {
+        if(numberInput ==='') return
+        const getRandomFact = async () => {
+            try {
+                const res = await axios.get(`http://numbersapi.com/${numberInput}`)
+                console.log(res)
+                setFact(res.data)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        getRandomFact()
+    }, [displayFact,numberInput])
+
+    return (
+        <h3>{fact}</h3>
+    )
+}
+
+export default Numbers
